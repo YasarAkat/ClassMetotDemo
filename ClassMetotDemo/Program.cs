237e818e3e24eecd0ba85573d6c503bb33a2186d
Yaşar Mehmet Akat <akat434@gmail.com>
2023-06-20 12:30:42 +0300

    Müşteri Bilgileri Çalışması

--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -43,10 +43,9 @@ namespace ClassMetotDemo
 
             musteriİslemleri.Sil(musteri1);
             musteriİslemleri.Sil(musteri2);
-            musteriİslemleri.Sil(musteri3);
+            
 
-            musteriİslemleri.Listele(musteri1);
-            musteriİslemleri.Listele(musteri2);
+            
             musteriİslemleri.Listele(musteri3);
 
             Console.WriteLine("Hello World!");
