9c26b92b32cf76142ef9108a16758b33fc811462
Yaşar Mehmet Akat <akat434@gmail.com>
2023-06-20 12:14:51 +0300

    Add project files.

---
+++ ClassMetotDemo/Musteriİslemleri.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class Musteriİslemleri
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Eklenen Müşteri :" + musteri.Adi + " " + musteri.Soyadi);
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Silinen Müşteri : " + musteri.Id);
+        }
+
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Müşteriler Listelendi : " + musteri.Id + " " + musteri.Adi + " " + musteri.Meslek);
+        } 
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Adi = "Elif";
+            musteri1.Soyadi = "Akat";
+            musteri1.Meslek = "Öğretmen";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Adi = "Yaşar Mehmet";
+            musteri2.Soyadi = "Akat";
+            musteri2.Meslek = "Yazılımcı";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Adi = "Azra";
+            musteri3.Soyadi = "Akat";
+            musteri3.Meslek = "Pilot";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3};
+
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("Müşteri ID : " + musteri.Id);
+                Console.WriteLine("Müşteri Adı : " + musteri.Adi);
+                Console.WriteLine("Müşteri Soyadı : " + musteri.Soyadi);
+                Console.WriteLine("Müşteri Mesleği : " + musteri.Meslek);
+                Console.WriteLine("------------------------");
+            }
+
+            Console.WriteLine("----------MÜŞTERİ EKLEME-SİLME-LİSTELEME İŞLEMLERİ-----------");
+            Musteriİslemleri musteriİslemleri = new Musteriİslemleri();
+            musteriİslemleri.Ekle(musteri1);
+            musteriİslemleri.Ekle(musteri2);
+            musteriİslemleri.Ekle(musteri3);
+
+            musteriİslemleri.Sil(musteri1);
+            musteriİslemleri.Sil(musteri2);
+            musteriİslemleri.Sil(musteri3);
+
+            musteriİslemleri.Listele(musteri1);
+            musteriİslemleri.Listele(musteri2);
+            musteriİslemleri.Listele(musteri3);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
